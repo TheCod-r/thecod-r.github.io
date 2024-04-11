6c3ca710fd8b7e3b33b316d2a7b40cde96085032
I forgot my name... *SNIFF* <76514524+TheCod-r@users.noreply.github.com>
2024-04-11 22:36:29 +0700

    Create script.js
    Yes

---
+++ script.js
@@ -0,0 +1,3 @@
+const $ = q => document.querySelector(q);
+const projects = [["RCGG", "Random Chess Game Generator ♟️"], ["Old-Word-Finder", "Word Finder [OLD] 📻"], ["Word-Finder", "Word Finder [NEW] 🔍"]];
+projects.forEach(project => $("#projects").innerHTML += `<li><a href="\\${project[0]}\\">${project[1]}</a></li>`);
